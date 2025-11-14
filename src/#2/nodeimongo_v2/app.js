e4b26983f9a68993b136de82cb575315563d56b7
Dawk0 <dawkoplus@gmail.com>
2025-11-14 09:52:13 +0100

    14.11.2025 programming

--- src/#2/nodeimongo_v2/app.js
+++ src/#2/nodeimongo_v2/app.js
@@ -21,3 +21,30 @@ const ksiazkiSchema = new mongoose.Schema({
 const ksiazkiModel = mongoose.model('Ksiazki', ksiazkiSchema, 'ksiazki');
 
 
+//Przekazanie plików z folderu public
+app.use(express.static(path.join(__dirname,'public')));
+
+mongoose.connect(baza)
+    .then(()=>{
+        console.log('Połączono z bazą');
+        //trasa główna - zwraca index.html
+        app.get('/', async (req, res) => {
+            res.sendFile(path.join(__dirname, 'public', 'index.html'))
+        });
+
+        //pobranie z bazy
+        app.get('/api/ksiazki', async(req,res)=> {
+            try{
+                const ksiazki = await ksiazkiModel.find({});
+                res.json(ksiazki);
+            } catch(err) {
+                console.error(err);
+                res.status(500).json({error: 'Błąd pobierania danych'});
+            }
+        });
+        //uruchomienie serwera
+        app.listen(5555, ()=>{
+            console.log('Serwer działa na porcie 5555');
+        });
+    })
+    .catch(err => console.error('Błąd połączenia z bazą', err));
